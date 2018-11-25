f8842f57040e5b470b80f4ffbfaeedc0b9f466f1
Caio Marcellos <caiocuritiba@gmail.com>
2018-11-25 19:32:13 -0200

    Not sure how to organize the data, SHOULD UPLOAD, I will start Not uploading to python, it will be handled in JS

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,16 @@
 # import main Flask class and request object
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, request, jsonify, flash, redirect, url_for
+from flask import send_from_directory
+from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
+import config
+import utils
+import numpy as np
 
 app = Flask(__name__)  # create the Flask app
 CORS(app)
+app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
 
 @app.route('/initialize', methods=['POST'])
 def initialize():
@@ -20,24 +27,27 @@ def initialize():
 @app.route('/file-upload', methods=['POST'])
 def file_upload():
     print('Reached py!')
-    # req_data = request.get_json()
-    # req_form = request.form.to_dict()
-    # print(req_data)
-    # print(req_form)
-    # print(request.form)
-    # print(request)
-    print(request.files)
-    print(request.files.get('file'))
-    # print(request.get_data())
-    # print(request.stream.read())
-    # mso = req_data['mso']
-    # flw = req_data['flowsheet']
-    # res = {
-    #     'msg': 'EMSO Wrapper successfull initialized',
-    #     'status': 0,
-    # }
-    # return jsonify(res)
+
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and utils.allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        data_read = np.genfromtxt(file, delimiter=',', names=True)
+        print(data_read.dtype.names)
+        return 'Done'
+
     return '<h1>Hello CORS</h1>'
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
 
 
 if __name__ == '__main__':
