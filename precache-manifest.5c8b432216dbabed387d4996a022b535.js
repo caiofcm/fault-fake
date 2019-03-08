2089ffc7f978d2185012ab05e87f613561907978
Caio Curitiba Marcellos <caiocuritiba@gmail.com>
2019-03-08 16:39:32 -0300

    homepage package and deploy gh pages

---
+++ precache-manifest.5c8b432216dbabed387d4996a022b535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32c02d83fc400e268bc5249705738638",
+    "url": "/fault-fake/static/media/android-chrome-512x512.32c02d83.png"
+  },
+  {
+    "revision": "486e3316494497645ea1",
+    "url": "/fault-fake/static/js/runtime~main.486e3316.js"
+  },
+  {
+    "revision": "e84eebfd9ee138311c88",
+    "url": "/fault-fake/static/js/main.e84eebfd.chunk.js"
+  },
+  {
+    "revision": "ccac2b66ae08fcd18e99",
+    "url": "/fault-fake/static/js/1.ccac2b66.chunk.js"
+  },
+  {
+    "revision": "e84eebfd9ee138311c88",
+    "url": "/fault-fake/static/css/main.b97e6cda.chunk.css"
+  },
+  {
+    "revision": "7ab2414c67e3fcb529f23bd03dc78b52",
+    "url": "/fault-fake/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6d550037186659ca71f569d62965873.js"
+  "/fault-fake/precache-manifest.5c8b432216dbabed387d4996a022b535.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/fault-fake/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
